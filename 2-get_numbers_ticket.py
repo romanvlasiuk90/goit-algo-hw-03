cbf611752727520c5a74edc0a4ccecf4143e7d3f
7777777&Aa <romanvlasiuk90@gmail.com>
2024-04-21 15:16:16 +0300

    #2 programmed and tested

---
+++ 2-get_numbers_ticket.py
@@ -0,0 +1,17 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    # Перевірка валідності вхідних даних
+    if min >= 1 and max <= 1000 and max > min and quantity > 0:
+        # Оголошуємо множину і наповнюємо по к-сті чисел лотереї
+        numbers_set = set()
+        while len(numbers_set) < quantity:
+            # Додаємо рандомні числа у множину
+            numbers_set.add(random.randint(min, max))
+        # Відсортовуємо + вивід
+        return print(f"Ваші лотерейні числа: {sorted(numbers_set)}")
+    else:
+        return print(f"Помилка вводу умови лотереї")
+
+# Умова Лотереії - Min , Max, Quantity
+lottery_numbers = get_numbers_ticket(1, 36, 5)
